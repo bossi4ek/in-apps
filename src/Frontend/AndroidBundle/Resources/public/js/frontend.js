e7400898f4474fc89232c2e4e6a8a30163809b6a
bossi4ek <bossi4ek@gmail.com>
2014-04-08 20:09:01 +0300

    Added pirobox gallery

--- src/Frontend/AndroidBundle/Resources/public/js/frontend.js
+++ src/Frontend/AndroidBundle/Resources/public/js/frontend.js
@@ -21,6 +21,16 @@ action = function(obj) {
     });
 };
 
-$(document).ready(function() {
+initGallery = function() {
+    $().piroBox({
+        my_speed: 400, //animation speed
+        bg_alpha: 0.1, //background opacity
+        slideShow : true, // true == slideshow on, false == slideshow off
+        slideSpeed : 4, //slideshow duration in seconds(3 to 6 Recommended)
+        close_all : '.piro_close,.piro_overlay'// add class .piro_overlay(with comma)if you want overlay click close piroBox
+    });
+};
 
+$(document).ready(function() {
+    initGallery();
 });
